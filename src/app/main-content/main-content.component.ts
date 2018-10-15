4e9ee452bd5f2bc559ec3d669b20bc9a6b703121
jonathanzepeda <jaydotzee@mail.fresnostate.edu>
2018-10-14 23:53:47 -0700

    First Commit

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { unescapeHtml } from '@angular/platform-browser/src/browser/transfer_state';
 
+interface IStudent {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  Course: string;
+}
 @Component({
   selector: 'main-content',
   templateUrl: './main-content.component.html',
@@ -7,9 +14,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
-  constructor() { }
+  students: Array<IStudent> = [];
+
+  constructor() {
+    this.students[0] = {
+      id: 1,
+      firstName: 'Jonathan',
+      lastName: 'Zepeda',
+      Course: 'Programming'
+    };
+    this.students[1] = {
+      id: 1,
+      firstName: 'Brooke',
+      lastName: 'Herrera',
+      Course: 'Nursing'
+    };
+    this.students[2] = {
+      id: 1,
+      firstName: 'Melissa',
+      lastName: 'Zepeda',
+      Course: 'Psychology'
+    };
+  }
 
   ngOnInit() {
   }
+
+addStudent() {
+  const student = {
+    id: 1,
+    firstName: 'Kenny',
+    lastName: 'Keys',
+    Course: 'Engineering'
+  };
+  this.students.push(student);
+}
 
 }
